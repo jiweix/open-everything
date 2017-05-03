35706e5aa820887237abe79c8ccb1eedc0719c5b
Jiwei Xu <jx734@nyu.edu>
2017-05-03 10:51:42 -0400

    data model ready

--- app/models.py
+++ app/models.py
@@ -17,33 +17,106 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+association_table = db.Table('association', db.Model.metadata,
+    db.Column('resource_id', db.Integer, db.ForeignKey('resource.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+)
+
 class Resource(db.Model):
-    __tablename__ = "resources"
+    __tablename__ = "resource"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    owner_id = db.Column(db.Integer)
-    description = db.Column(db.String(200))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # Integer value from 0 to 1440 in minutes for start_time and end_time
+    available_start = db.Column(db.Integer)
+    available_end = db.Column(db.Integer)
+    tags = db.relationship('Tags', secondary=association_table, backref='resource')
+    reservations = db.relationship('Reservation', backref='resource',
+                                lazy='dynamic')
 
     def serialize(self):
-        return { "id": self.id, "name": self.name, "owner_id": self.owner_id, "description": self.description}
+        return { "id": self.id,
+                 "name": self.name,
+                 "owner_id": self.owner_id,
+                 "available_start": self.available_start,
+                 "available_end": self.available_end
+                }
 
     def deserialize(self, data):
         try:
             self.name = data['name']
             self.owner_id = int(data['owner_id'])
-            self.description = data['description']
+            self.available_start = int(data['available_start'])
+            self.available_end = int(data['available_end'])
         except KeyError as e:
             raise KeyError('Invalid resource: missing ' + e.args[0])
         except TypeError as e:
-            raise KeyError('Invalid resource: body of request contained bad or no data')
+            raise TypeError('Invalid resource: body of request contained bad or no data')
         return self
 
-'''
+
 class Reservation(db.Model):
     __tablename__ = "reservation"
     id = db.Column(db.Integer, primary_key=True)
-    resource_id = db.Column(db.String(100))
-    user_id = db.Column(db.Integer)
-    start_time = db.Column(db.String(200))
-    end_time =
-    '''
+    resource_id = db.Column(db.Integer, db.ForeignKey('resource.id'))
+    resource_name = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # time should be integer as minutes
+    start_time = db.Column(db.Integer)
+    duration = db.Column(db.Integer)
+
+    def serialize(self):
+        return { "id": self.id,
+                 "resource_id": self.resource_id,
+                 "resource_name": self.resource_name,
+                 "user_id": self.user_id,
+                 "start_time": self.start_time,
+                 "duration": self.duration
+                }
+
+    def deserialize(self, data):
+        try:
+            self.resource_id = int(data['resource_id'])
+            self.resource_name = data['resource_name']
+            self.user_id = int(data['user_id'])
+            self.start_time = int(data['start_time'])
+            self.duration = int(data['duration'])
+        except KeyError as e:
+            raise KeyError('Invalid reservation: missing ' + e.args[0])
+        except TypeError as e:
+            raise TypeError('Invalid reservation: body of request contained bad or no data')
+        return self
+
+class Tag(db.Model):
+    __tablename__ = "tag"
+    id = db.Column(db.Integer, primary_key=True)
+    # lower case letters less than or equals to 20 chars
+    value = db.Column(db.String(20))
+
+    def __init__(value):
+        self.value = value
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100))
+    passhash = db.Column(db.String)
+    authenticated = db.Column(db.Boolean, default=False)
+    reservations = db.relationship('Reservation', backref='user',
+                                lazy='dynamic')
+    resources = db.relationship('Resource', backref='user',
+                                lazy='dynamic')
+
+    def __init__(email, passhash):
+        self.email = email
+        self.passhash = passhash
+
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        return self.id
+
+    def is_authenticated(self):
+        return self.authenticated
