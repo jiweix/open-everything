8b1c14c076a73f68a6ac3447bfd3d62527da8efe
Jiwei Xu <jx734@nyu.edu>
2017-05-05 16:00:42 -0400

    hot fix for heroic

--- run.py
+++ run.py
@@ -40,4 +40,6 @@ port = os.getenv('PORT', '8080')
 db.init_app(app)
 with app.app_context():
     db.create_all()
-app.run(host='0.0.0.0', port=int(port), debug=debug)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(port), debug=debug)
